414a2666cce8b56ef0224be210746f6fc8816762
Guillermo Merino <guillermo.merino@incotel.es>
2024-09-16 18:06:14 +0200

    fix(Root): get user lider roles on clan commands

--- utils/clanes/is-lider.js
+++ utils/clanes/is-lider.js
@@ -1,3 +1,7 @@
-export default function (user, clan) {
-	return clan.liders.includes(user.id)
+import ClanesConfig from "../../config/clanes.config.js"
+
+export default async function (user, clan) {
+	const config = await ClanesConfig.get()
+	const { lider_roles } = config
+	return clan.liders.includes(user.id) || lider_roles.some((role) => user.roles.cache.has(role))
 }
